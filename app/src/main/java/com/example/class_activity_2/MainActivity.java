c4a9ed3fcae7627da6f36a2333fd156039cd0906
SajedAbdAlkareem <sajedbaqa130@gmail.com>
2023-01-03 12:51:25 +0200

    activity2

---
+++ app/src/main/java/com/example/class_activity_2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.class_activity_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
